385c156c286f320bb92075687f0c2036e37a06ac
Synthbot Anon <synthbot.dump@gmail.com>
2021-06-29 02:56:14 -0700

    config files for compiling js to jsfl

---
+++ animate-scripts/webpack.config.js
@@ -0,0 +1,39 @@
+const commandsDir = `${__dirname}/../runner/animate-scripts-dist/`;
+
+const path = require('path');
+const fs = require('fs');
+
+const entries = {};
+fs.readdirSync(`${__dirname}/src/`).forEach((filename) => {
+  if (!filename.endsWith('.js')) {
+    return;
+  }
+  const name = filename.replace(/\.[^/.]+$/, "");
+  const path = `${__dirname}/src/${filename}`;
+  entries[name] = path;
+})
+
+module.exports = {
+  entry: entries,
+  output: {
+    filename: '[name].jsfl',
+    path: commandsDir
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?|\.js$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules')
+    ]
+  },
+  target: ['es3']
+};
